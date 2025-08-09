509754846831cbd9ed182a931006e80f35d395cd
sravanthimusuluri <sravanthimusuluri1910@gmail.com>
2025-08-09 19:24:39 +0530

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -10,39 +10,39 @@ class App extends Component {
         </header>
         <section>
           <div className='card'>
-            <img src={`{$BASEURL}1.jpeg`} alt='' />
+            <img src={`{$BASEURL}/1.jpeg`} alt='' />
             <label>Image 01</label>
           </div>
           <div className='card'>
-            <img src={`{$BASEURL}2.jpeg`} alt='' />
+            <img src={`{$BASEURL}/2.jpeg`} alt='' />
             <label>Image 02</label>
           </div>
           <div className='card'>
-            <img src={`{$BASEURL}3.jpeg`} alt='' />
+            <img src={`{$BASEURL}/3.jpeg`} alt='' />
             <label>Image 03</label>
           </div>
           <div className='card'>
-            <img src={`{$BASEURL}4.jpeg`} alt='' />
+            <img src={`{$BASEURL}/4.jpeg`} alt='' />
             <label>Image 04</label>
           </div>
           <div className='card'>
-            <img src={`{$BASEURL}5.jpeg`} alt='' />
+            <img src={`{$BASEURL}/5.jpeg`} alt='' />
             <label>Image 05</label>
           </div>
           <div className='card'>
-            <img src={`{$BASEURL}6.jpeg`} alt='' />
+            <img src={`{$BASEURL}/6.jpeg`} alt='' />
             <label>Image 06</label>
           </div>
           <div className='card'>
-            <img src={`{$BASEURL}7.jpeg`} alt='' />
+            <img src={`{$BASEURL}/7.jpeg`} alt='' />
             <label>Image 07</label>
           </div>
           <div className='card'>
-            <img src={`{$BASEURL}8.jpeg`} alt='' />
+            <img src={`{$BASEURL}/8.jpeg`} alt='' />
             <label>Image 08</label>
           </div>
           <div className='card'>
-            <img src={`{$BASEURL}10.jpeg`} alt='' />
+            <img src={`{$BASEURL}/10.jpeg`} alt='' />
             <label>Image 10</label>
           </div>
         </section>
